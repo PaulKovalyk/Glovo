b310f14dc55f82121a38e06fcab5b94c6bc5e2e8
Paul Ruby <kolodiy6767@gmail.com>
2022-06-30 16:09:16 +0300

    add styles, notice update dont work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-
-    protected
-  
-  
+   
     
+    protected
+       
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:email])
-  end
+    end
 
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
 end
