b10f26bb3aebc0b256c5daf83917fef0daeadc29
Paul Ruby <kolodiy6767@gmail.com>
2022-07-01 09:45:42 +0300

    fix rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "update" do
-    let(:user) {create(:user) }
+  describe 'update' do
+    let(:user) { create(:user) }
     let(:first_name) { 'Alex' }
     let(:last_name) { 'Doe' }
 
-    it 'updates user' do 
-      expect(user.first_name).to eq(nil)
+    it 'updates user' do
       user.update(first_name: first_name)
       expect(user.reload.first_name).to eq('Alex')
     end
