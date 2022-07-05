f13f4cf33a5e86138f9af3b3edd1f59926ddacd2
Paul Ruby <kolodiy6767@gmail.com>
2022-07-05 17:29:38 +0300

    RUbocop fix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,13 @@
-class Restaurant < ApplicationRecord
-    has_many :restaurant_tags, dependent: :destroy
-    has_many :tags, through: :restaurant_tags
-    has_many :dishes, dependent: :destroy
-    validates :name, :description, :image_url, :address, presence: true
-    validates :name, uniqueness: true, length: {minimum: 2}
-    validates :image_url,  allow_blank: true, format: { 
-        with: %r{\.(gif|jpg|png|svg)\Z}i,
-        message: 'URL має вказувати на зображення формату GIF, JPG, чи PNG'}
+# frozen_string_literal: true
 
+class Restaurant < ApplicationRecord
+  has_many :restaurant_tags, dependent: :destroy
+  has_many :tags, through: :restaurant_tags
+  has_many :dishes, dependent: :destroy
+  validates :name, :description, :image_url, :address, presence: true
+  validates :name, uniqueness: true, length: { minimum: 2 }
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|jpg|png|svg)\Z/i,
+    message: 'URL має вказувати на зображення формату GIF, JPG, чи PNG'
+  }
 end
