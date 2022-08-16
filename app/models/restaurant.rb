2302919469e51b8450a89762b165ccfc3053a873
Paul Ruby <kolodiy6767@gmail.com>
2022-08-16 12:00:12 +0300

    deleting public files

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Restaurant < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  mount_uploader :image, RestaurantUploader
   has_many :restaurant_tags, dependent: :destroy
   has_many :tags, through: :restaurant_tags
   has_many :dishes, dependent: :destroy
--- app/uploaders/restaurant_uploader.rb
+++ app/uploaders/restaurant_uploader.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-class ImageUploader < CarrierWave::Uploader::Base
+class RestaurantUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -37,9 +35,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_allowlist
-    %w[jpg jpeg gif png]
-  end
+  # def extension_allowlist
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
