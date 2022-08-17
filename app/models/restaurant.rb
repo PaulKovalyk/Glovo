a1dcc38386b213e4042a2f4ba689d6977dc10edb
Paul Ruby <kolodiy6767@gmail.com>
2022-08-17 11:00:06 +0300

    add search with tags

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,4 +8,16 @@ class Restaurant < ApplicationRecord
   belongs_to :user
   validates :name, :description, :address, presence: true
   validates :name, length: { minimum: 2 }
+
+ 
+  scope :all_by_tags, lambda { |tags|
+    restaurants = includes(:user)
+    restaurants = if tags
+      restaurants.joins(:tags).where(tags: tags)
+                else
+                  restaurants.includes(:restaurant_tags, :tags)
+                end
+
+                restaurants.order(created_at: :desc)
+  }
 end
